97829dd6399c7592fc49ddd0b3da8138d4d740b7
dq212 <>
2020-03-23 19:18:54 -0400

    ok

---
+++ build/precache-manifest.37a1d77c8b8c70af51c874efe577b27b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a550825ab5eb9e9337aecca4ab3c0c",
+    "url": "/2020-react/index.html"
+  },
+  {
+    "revision": "42c08f677b48aa20b550",
+    "url": "/2020-react/static/css/main.e45c9e73.chunk.css"
+  },
+  {
+    "revision": "ec0e41b1b5612bfabadd",
+    "url": "/2020-react/static/js/2.87fb8b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2020-react/static/js/2.87fb8b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c08f677b48aa20b550",
+    "url": "/2020-react/static/js/main.dd17fe0a.chunk.js"
+  },
+  {
+    "revision": "028e540cd946b97a2a80",
+    "url": "/2020-react/static/js/runtime-main.86a6ff04.js"
+  },
+  {
+    "revision": "a7a677735c96af71e75c90abbe4cc5c5",
+    "url": "/2020-react/static/media/puako.a7a3da66.jpg"
+  }
+]);
