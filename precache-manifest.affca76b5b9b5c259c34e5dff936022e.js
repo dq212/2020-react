18694b521e10eb5ca4a2302126c69761c262ad06
dq212 <dqsignup@gmail.com>
2020-03-23 23:00:20 -0400

    build 2

---
+++ precache-manifest.affca76b5b9b5c259c34e5dff936022e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5dc0df5d4b1bf2879fb68246fc1984",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1a689bf7b9ec2f13bc4f",
+    "url": "./static/css/main.19ba6421.chunk.css"
+  },
+  {
+    "revision": "ec0e41b1b5612bfabadd",
+    "url": "./static/js/2.87fb8b80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.87fb8b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a689bf7b9ec2f13bc4f",
+    "url": "./static/js/main.bf30bace.chunk.js"
+  },
+  {
+    "revision": "1dcfaf1e945c4aaf6d4e",
+    "url": "./static/js/runtime-main.bcc5c4a5.js"
+  },
+  {
+    "revision": "a7a677735c96af71e75c90abbe4cc5c5",
+    "url": "./static/media/puako.a7a67773.jpg"
+  }
+]);
